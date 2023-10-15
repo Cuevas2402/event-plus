856e85e2a08a5fe74ac5b24bfc7ba605ac9d1d16
cuevas <juan.m.cuevas@outlook.com>
2023-10-15 16:20:29 -0600

    Modificar estilos

--- app/credentials.py
+++ app/credentials.py
@@ -1,16 +1,66 @@
 from app import app, mysql
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, session
 
 @app.route('/login')
 def login():
-    return render_template('pages/login.html')
+    if 'username' not in session:
+        return render_template('pages/login.html')
+    else:
+        return redirect(url_for('main_page'))
+
+@app.route('/logout')
+def log_out():
+    if 'username' in session:
+        session.pop('username', None)
+
+    if 'id' in session:
+        session.pop('id', None)
+
+    if 'logged' in session:
+        session.pop('logged', None)
+
+    return redirect(url_for('main_page'))
 
 @app.route('/register')
 def register():
-    return render_template('pages/register.html')
+    if 'username' not in session:
+        return render_template('pages/register.html')
+    else:
+        return redirect(url_for('main_page'))
 
-#@app.route('/iniciar')
-#def iniciar():
+@app.route('/iniciar', methods = ['GET', 'POST'])
+def iniciar():
+    if 'username' not in session:
+        if request.method == 'POST':
+            user = request.form.get('user') 
+            password = request.form.get('password')
+
+            cursor = mysql.connection.cursor()
+
+            cursor.execute('SELECT * FROM users WHERE email LIKE %s AND password LIKE %s;', (user, password,))
+           
+            results = cursor.fetchall()
+            
+            cursor.close()
+            
+            if len(results) > 0:
+
+                session['username'] = user
+
+                session['logged'] = True
+
+                session['id'] = results[0][0] 
+
+                return redirect(url_for('main_page'))
+
+            else:
+
+                return redirect(url_for('login'))
+
+
+    else:
+        return redirect(url_for('main_page'))
+    
 
 @app.route('/registrar', methods = ['GET', 'POST'])
 def registar_usuario():
@@ -20,7 +70,17 @@ def registar_usuario():
         
         cursor = mysql.connection.cursor()
 
-        cursor.execute('INSERT INTO users (email, password) VALUES (%s, %s,)', (email, password,))
-        mysql.connection.commit()
+        cursor.execute('SELECT * FROM users WHERE email = %s', (email, ))
 
-        return render_template('index.html')
+        results = cursor.fetchall()
+        if len(results) > 0:
+            cursor.close()
+            return redirect(url_for('register'))
+        else:
+            cursor.execute('INSERT INTO users (email, password) VALUES (%s, %s)', (email, password,))
+            mysql.connection.commit()
+
+            cursor.close()
+    
+            return redirect(url_for('main_page')) 
+
