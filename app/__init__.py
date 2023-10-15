d8c32acbd6258d86a698ffabfe895e9342633e98
cuevas <juan.m.cuevas@outlook.com>
2023-10-15 09:56:10 -0600

    Añidir requerimientos

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 #load_dotenv()
 
-#app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-#app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-#app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-#app.config['MYSQL_DB'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_HOST'] = '127.0.0.1' 
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'soiree'
 
 mysql = MySQL(app)
 
-from app import main_page
+from app import main_page, credentials
---
+++ app/credentials.py
@@ -0,0 +1,26 @@
+from app import app, mysql
+from flask import render_template, request
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+#@app.route('/iniciar')
+#def iniciar():
+
+@app.route('/registrar', methods = ['GET', 'POST'])
+def registar_usuario():
+    if request.method == 'POST':
+        email =  request.form.get('user')
+        password = request.form.get('password')
+        
+        cursor = mysql.connection.cursor()
+
+        cursor.execute('INSERT INTO users (email, password) VALUES (%s, %s,)', (email, password,))
+        mysql.connection.commit()
+
+        return render_template('index.html')
