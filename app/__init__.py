37a89b93b996aa12f0894ff674c2a913781cf520
cuevas <juan.m.cuevas@outlook.com>
2023-10-15 10:13:23 -0600

    Modificar organizacion del directorio

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
 from flask_mysqldb import MySQL
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
+from flask_session import Session
 import os
 
 app = Flask(__name__)
 
-#load_dotenv()
 
-app.config['MYSQL_HOST'] = '127.0.0.1' 
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'soiree'
+app.secret_key = "nuestraClaveSecreta"
+load_dotenv('../.env')
+
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST') 
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
